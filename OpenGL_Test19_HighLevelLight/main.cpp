174e6d577a7b434bf2a07e5f91b4f1142ff74c65
LiangHaoXiang <1013568397@qq.com>
2021-08-05 13:24:12 +0800

    Initial Commit

---
+++ OpenGL_Test19_HighLevelLight/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  OpenGL_Test19_HighLevelLight
+//
+//  Created by Haoxiang Liang on 2021/8/5.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
